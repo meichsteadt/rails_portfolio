35d448db480da495fe4ed62ee11e6dd220827f63
Matt Eichsteadt <matteichsteadt@gmail.com>
2017-03-03 01:07:49 -0800

    ensure Oauth only accepts me

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,10 +1,18 @@
+require 'rest-client'
 class UserController < ApplicationController
   def new
     code = params[:code]
     @user = User.new()
     access_token = @user.post(code)["access_token"]
-    session[:access_token] = access_token
-    flash[:notice] = "You are now logged in"
-    redirect_to landing_index_path
+    user = JSON.parse(RestClient.get('https://api.github.com/user',
+                  {:params => {:access_token => access_token}}))
+    if user["login"] === "meichsteadt"
+      session[:access_token] = access_token
+      flash[:notice] = "You are now logged in"
+      redirect_to landing_index_path
+    else
+      flash[:notice] = "You are not me"
+      redirect_to '/'
+    end
   end
 end
